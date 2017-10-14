4232df1ee998c4c084a4b999481c56cfcb06eb9f
Renato Marinho <renato.marinho@s2move.com>
2017-10-14 13:39:50 +0100

    update html and tests

--- tests/Middleware/ElideAttributesTest.php
+++ tests/Middleware/ElideAttributesTest.php
@@ -2,7 +2,6 @@
 
 namespace RenatoMarinho\LaravelPageSpeed\Test\Middleware;
 
-
 use RenatoMarinho\LaravelPageSpeed\Middleware\ElideAttributes;
 use RenatoMarinho\LaravelPageSpeed\Test\TestCase;
 
@@ -19,8 +18,8 @@ class ElideAttributesTest extends TestCase
 
         $this->assertContains('<input type="text" disabled value="teste" width="100%">', $response->getContent());
         $this->assertContains('<input type="text" disabled>', $response->getContent());
-        $this->assertContains('<option selected class="selected">Item</option>', $response->getContent());
+        $this->assertContains('<option selected class="selected" style="cursor: default">', $response->getContent());
         $this->assertContains('<button name="ok" disabled class="btn" style="border:3px solid blue;">OK</button>', $response->getContent());
-        $this->assertContains('<form style="display:block;border:1px solid red;">', $response->getContent());
+        $this->assertContains('<form class="form" style="display:block;border:1px solid red;">', $response->getContent());
     }
 }
