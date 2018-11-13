832dcf9c276e99f6508edd6e4ba1cf94fc22ad5a
João Roberto P. Borges <joaorobertopb@gmail.com>
2018-11-12 23:20:46 -0200

    Fix remove JS and CSS comments

--- tests/Middleware/RemoveCommentsTest.php
+++ tests/Middleware/RemoveCommentsTest.php
@@ -7,38 +7,121 @@ use RenatoMarinho\LaravelPageSpeed\Test\TestCase;
 
 class RemoveCommentsTest extends TestCase
 {
+    protected $response;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->response = $this->middleware->handle($this->request, $this->getNext());
+    }
+
     protected function getMiddleware()
     {
         $this->middleware = new RemoveComments();
     }
 
-    public function testRemoveComments()
+    public function testRemoveHtmlComments()
     {
-        $response = $this->middleware->handle($this->request, $this->getNext());
-
         $this->assertNotContains(
             "<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->",
-            $response->getContent()
+            $this->response->getContent()
         );
 
         $this->assertContains(
             "<!--[if IE 8]>",
-            $response->getContent()
+            $this->response->getContent()
         );
 
         $this->assertContains(
             "<!--[if !IE]><!-->",
-            $response->getContent()
+            $this->response->getContent()
         );
 
         $this->assertContains(
             "<!--<![endif]-->",
-            $response->getContent()
+            $this->response->getContent()
+        );
+    }
+
+    public function testRemoveCssComments()
+    {
+        $this->assertNotContains(
+            "/* This is a single-line css comment */",
+            $this->response->getContent()
+        );
+
+        $this->assertNotContains(
+            "/* This is
+                a multi-line
+                css comment */",
+            $this->response->getContent()
         );
 
         $this->assertContains(
-            "<![endif]-->",
-            $response->getContent()
+            "<!--<![endif]-->",
+            $this->response->getContent()
+        );
+
+        $this->assertContains(
+            '.laravel-page-speed',
+            $this->response->getContent()
+        );
+
+        $this->assertContains(
+            'text-align: center;',
+            $this->response->getContent()
+        );
+
+        $this->assertContains(
+            'color: black;',
+            $this->response->getContent()
+        );
+    }
+
+    public function testRemoveJsComments()
+    {
+        $this->assertNotContains(
+            "// Single Line Comment",
+            $this->response->getContent()
+        );
+
+        $this->assertNotContains(
+            "/*
+            Multi-line1
+            Comment
+        */",
+            $this->response->getContent()
+        );
+
+        $this->assertNotContains(
+            "// Single Line Comment",
+            $this->response->getContent()
+        );
+
+        $this->assertNotContains(
+            "/* before - inline comment*/",
+            $this->response->getContent()
+        );
+
+        $this->assertNotContains(
+            "// after - inline comment",
+            $this->response->getContent()
+        );
+
+        $this->assertContains(
+            "console.log('Laravel');",
+            $this->response->getContent()
+        );
+
+        $this->assertContains(
+            "console.log('Page');",
+            $this->response->getContent()
+        );
+
+        $this->assertContains(
+            "console.log('Speed!');",
+            $this->response->getContent()
         );
     }
 }
