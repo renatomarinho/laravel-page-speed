ae8f7ffa013d440623a97e40dac9a66ceaed10f8
Diego Lozano <jferdi24@users.noreply.github.com>
2018-11-13 14:14:06 -0500

    Update RemoveComments.php

--- src/Middleware/RemoveComments.php
+++ src/Middleware/RemoveComments.php
@@ -7,7 +7,9 @@ class RemoveComments extends PageSpeed
     public function apply($buffer)
     {
         $replace = [
-            '/<!--[^]><!\[](.*?)[^\]]-->/s' => ''
+            '/<!--[^]><!\[](.*?)[^\]]-->/s' => '',
+            '/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/m' => '$1',
+            '/\h*\/\*.*?\*\/\h*/s' => '',
         ];
 
         return $this->replace($replace, $buffer);
