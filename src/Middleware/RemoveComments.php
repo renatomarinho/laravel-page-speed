e112b6632bc85dcaca79dedb09c8a370045d155b
João Roberto P. Borges <joaorobertopb@gmail.com>
2018-11-12 23:20:46 -0200

    Fix remove JS and CSS comments

--- src/Middleware/RemoveComments.php
+++ src/Middleware/RemoveComments.php
@@ -7,7 +7,8 @@ class RemoveComments extends PageSpeed
     public function apply($buffer)
     {
         $replace = [
-            '/<!--[^]><!\[](.*?)[^\]]-->/s' => ''
+            '/<!--[^]><!\[](.*?)[^\]]-->/s' => '',
+            '/(?:(?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:(?<!\:|\\\|\'|\")\/\/.*))/' => '',
         ];
 
         return $this->replace($replace, $buffer);
