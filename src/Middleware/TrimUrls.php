f45622780db59e31de0796236387e902fa8e392b
Ninth <quarpoe@gmail.com>
2023-08-10 01:24:16 +0300

    docs: add PHPDocs to TrimUrls middleware

--- src/Middleware/TrimUrls.php
+++ src/Middleware/TrimUrls.php
@@ -6,13 +6,17 @@ namespace RenatoMarinho\LaravelPageSpeed\Middleware;
 
 class TrimUrls extends PageSpeed
 {
+    /**
+     * Trim URLs from the given buffer
+     *
+     * @param  string  $buffer The input buffer to apply transformations to
+     * @return string The resulting buffer after applying transformations
+     */
     public function apply(string $buffer): string
     {
-        $replace = [
+        return $this->replace([
             '/https:/' => '',
             '/http:/' => '',
-        ];
-
-        return $this->replace($replace, $buffer);
+        ], $buffer);
     }
 }
