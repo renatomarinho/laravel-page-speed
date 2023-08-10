13b2afdaccb2ea7f170c0f7ab7a6c9b41d1dbb12
Ninth <quarpoe@gmail.com>
2023-08-10 13:56:07 +0300

    test: add unit test for DeferJavascript middleware

---
+++ tests/Unit/Middleware/DeferJavascriptTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DotNinth\LaravelPageSpeed\Middleware;
+
+use Pest\TestSuite\TestCase;
+
+it("should defer JavaScript in the HTML buffer", function () {
+    $deferJavascript = new DeferJavascript();
+    $buffer = '<html><head><script src="script.js"></script></head><body></body></html>';
+
+    $expected = '<html><head><script src="script.js" defer></script></head><body></body></html>';
+
+    $actual = $deferJavascript->apply($buffer);
+
+    expect($actual)->toBe($expected);
+});
